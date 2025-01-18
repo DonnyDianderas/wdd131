4d8f186cbc135fd3e432d023c3043e7aa1e58efe
DonnyDianderas <donnydianderas@gmail.com>
2025-01-18 11:58:46 -0500

    add events to program

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,13 +1,32 @@
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
-const list = document.querySelector('_________');
+const list = document.querySelector('#list');
 
-const li = document.createElement('li');
-const deleteButton = document.createElement('button');
+button.addEventListener('click', function(){
 
-li.textContent = input.value;
-deleteButton.textContent = '❌';
+    if(input.value.trim()!==''){
+        const li = document.createElement('li');
+        const deleteButton = document.createElement('button');
 
-li.append(deleteButton);
-list.append(li);
+        li.textContent = input.value;
+        deleteButton.textContent = '❌';
+
+        li.append(deleteButton);
+        list.append(li);
+
+        deleteButton.addEventListener('click', function() {
+            list.removeChild(li);
+            input.focus();
+        });
+
+        input.value = '';
+
+        input.focus();
+    }
+
+    else{
+        input.focus();
+    }
+    
+});
 
